31da10ee604a56e2b0d2157c52ce086ec87461f6
chriscco <2444628489c@gmail.com>
2025-06-08 15:04:46 -0700

    task completed

--- project_247/src/main/java/Driver.java
+++ project_247/src/main/java/Driver.java
@@ -1,6 +1,23 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import server.WordCountServiceImpl;
+
+import java.io.IOException;
+
 public class Driver {
-    public static void main(String[] args) {
-        WordCounter counter = new WordCounter();
-        counter.counter();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        WordCountServiceImpl wordCountService = new WordCountServiceImpl();
+        Server server = ServerBuilder
+                .forPort(9091)
+                .addService(wordCountService)
+                .maxInboundMessageSize(1024*1024*20)
+                .build()
+                .start();
+        System.out.println("running on 9091");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("shutting down grpc server");
+            server.shutdown();
+        }));
+        server.awaitTermination();
     }
 }
--- project_247/src/main/java/server/WordCountServiceImpl.java
+++ project_247/src/main/java/server/WordCountServiceImpl.java
@@ -4,9 +4,96 @@ import WordCountService.ServerProto;
 import WordCountService.WordCountGrpc;
 import io.grpc.stub.StreamObserver;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class WordCountServiceImpl extends WordCountGrpc.WordCountImplBase {
+    private final ConcurrentHashMap<String, AtomicInteger> result = new ConcurrentHashMap<>();
+    private final ExecutorService executor = Executors.newFixedThreadPool(4);
+
+    /**
+     * count words in multi-thread mode
+     * @param responseObserver responseObserver
+     * @return StreamObserver
+     */
     @Override
-    public void wordCount(ServerProto.WordCountRequest request, StreamObserver<ServerProto.WordCountResponse> responseObserver) {
-        ServerProto.WordCountResponse response =
+    public StreamObserver<ServerProto.WordCountRequest> wordCount(StreamObserver<ServerProto.WordCountResponse> responseObserver) {
+        return new StreamObserver<ServerProto.WordCountRequest>() {
+            @Override
+            public void onNext(ServerProto.WordCountRequest wordCountRequest) {
+                executor.submit(() -> {
+                    String input = wordCountRequest.getRequestMessage();
+                    String key = keyGen(input);
+                    String[] words = input.split("\\s+");
+                    System.out.println("getRequestMessage: " + wordCountRequest.getRequestMessage() +
+                            "Key: " + key);
+
+                    for (String word : words) {
+                        result.computeIfAbsent(word, k -> new AtomicInteger(0))
+                                .incrementAndGet();
+
+                        ServerProto.WordCountResponse.Builder responseBuilder = ServerProto.WordCountResponse.newBuilder();
+                        for (Map.Entry<String, AtomicInteger> entry : result.entrySet()) {
+                            ServerProto.WordCountResponse.WordCountResult temp =
+                                    ServerProto.WordCountResponse.WordCountResult
+                                            .newBuilder()
+                                            .setWord(entry.getKey())
+                                            .setCount(entry.getValue().get())
+                                            .build();
+                            responseBuilder.addCt(temp);
+                            responseBuilder.setKey(key);
+                        }
+                        // System.out.println("sending one response");
+                        responseObserver.onNext(responseBuilder.build());
+                    }
+                });
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Error in gRPC Streaming: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    private String keyGen(String str) {
+        try {
+            MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+
+            byte[] hash = sha1.digest(str.getBytes());
+            String hashPrefix = bytesToHex(hash).substring(0, 16);
+            return String.format("word_count:%s", hashPrefix);
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+
+    private Map<String, AtomicInteger> getMap() {
+        return Collections.unmodifiableMap(result);
+    }
+
+    private void printMap() {
+        for (Map.Entry<String, AtomicInteger> entry : getMap().entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+        }
     }
 }
